5d5ca67d53ddc9f00c2008ca12f14e3deac42c51
Lamsauce <lilylam743@gmail.com>
2020-12-17 16:51:03 -0800

    added content in views/posts/show.ejs

--- server.js
+++ server.js
@@ -6,3 +6,9 @@ const MongoStore = require("connect-mongo")(session);
 const controllers = require("./controllers");
 
 const app = express();
+
+const PORT = 4000;
+app.set("view engine", "ejs");
+app.listen(PORT, function(){
+    console.log(`Blog Application is live at http://localhost:${PORT}/`)
+  });
