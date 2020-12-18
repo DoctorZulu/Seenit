fd3adf9c6baf188a71c8f52e0569baa659ddac4f
Dan <neveloffd@gmail.com>
2020-12-18 12:44:06 -0500

    localhost est

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const router = express.Router();
 const db = require("../models");
 const { post } = require("./auth");
-
+//const authRequired = require("./middleware/authRequired");
 
 // all category index
 router.get("/", async function(req,res){
--- server.js
+++ server.js
@@ -6,3 +6,73 @@ const MongoStore = require("connect-mongo")(session);
 const controllers = require("./controllers");
 
 const app = express();
+
+const PORT = 4001;
+
+app.set("view engine", "ejs"); 
+/* ==== Middleware ==== */
+
+// server public as static files
+// express.static(directory location absolute)
+app.use(express.static(__dirname + '/public'));
+
+// body data middleware
+app.use(express.urlencoded({ extended: true }));
+// method override middleware
+app.use(methodOverride("_method"));
+
+// Session
+app.use(
+  session(
+    {
+      // set the store to the MongoStore we required
+      store: new MongoStore({
+        url: "mongodb://localhost:27017/seenit"
+      }),
+      // our secret is a signature in our sessions to verify that it is valid
+      secret: "Lamsauce the Great",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 2 // two weeks
+      }
+    }
+  )
+);
+
+app.use(function(req,res,next){
+ 
+  req.session.test = "Test 1234"
+  console.log(req.session);
+  next();
+});
+
+  //auth function
+
+app.use(function(req,res,next){
+  app.locals.user =  req.session.currentUser;
+  next();
+})
+  
+  
+  
+
+app.get("/", function(req, res){
+    // .render(file,context)
+    // const context = { user: req.session.currentUser }
+    res.render("home");
+});
+  
+  // Auth controller
+app.use("/", controllers.auth);
+
+//app.use("/authors", controllers.authors);
+
+// article controller
+app.use("/posts", controllers.posts);
+
+/* ==== Server Listener  ==== */
+app.listen(PORT, function(){
+  console.log(`Blog Application is live at http://localhost:${PORT}/`)
+});
+  
