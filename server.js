53924845d3b72961af097bdefda1525436ff417b
Lamsauce <lilylam743@gmail.com>
2020-12-18 09:57:40 -0800

    Wont let me merge bc I did something in server.js, fixed

--- server.js
+++ server.js
@@ -6,9 +6,3 @@ const MongoStore = require("connect-mongo")(session);
 const controllers = require("./controllers");
 
 const app = express();
-
-const PORT = 4000;
-app.set("view engine", "ejs");
-app.listen(PORT, function(){
-    console.log(`Blog Application is live at http://localhost:${PORT}/`)
-  });
