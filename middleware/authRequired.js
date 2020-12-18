d004491b959d35491eddc2dfb4c42e384a20875b
Dan <neveloffd@gmail.com>
2020-12-18 13:01:18 -0500

    middleware authreq added

---
+++ middleware/authRequired.js
@@ -0,0 +1,9 @@
+module.exports = function(req,res,next){
+    // if a user sesssion exists continue
+    // if not redirect to login
+    if(req.session.currentUser){
+      next();
+    } else {
+      res.redirect("/login");
+    }
+  }
